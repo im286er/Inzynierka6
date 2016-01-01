cba12ce2e5606cff9b69fc134b1be86c18069050
czifumasa <lukasz.lenart912@gmail.com>
2016-01-01 18:28:03 +0100

    01-01-2016 18:26
    -usuwanie z listy obserwowanych

--- Bootstrap/src/AppBundle/Controller/ObserwujController.php
+++ Bootstrap/src/AppBundle/Controller/ObserwujController.php
@@ -25,7 +25,7 @@ class ObserwujController extends Controller
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
-        //Dodac ifa na czy zalogowany i czy juz obserwuje
+        //Dodac czy juz obserwuje
         $em = $this->getDoctrine()->getEntityManager();
         $Repository = $this->getDoctrine()
             ->getRepository('AppBundle:Oferty');
@@ -38,6 +38,26 @@ class ObserwujController extends Controller
         $em->flush();
 
         return $this->redirectToRoute('_oferta',array('idOferty' => $idOferty));
+
+    }
+    /**
+     * @Route("/oferta/{idOferty}/nieobserwuj", name="App_Nieobserwuj")
+     */
+    public function deleteAction($idOferty){
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $oferta = $em->getRepository('AppBundle:Obserwowane')->findOneBy(
+            array('oferta' => $idOferty,
+                  'user' => $user)
+        );
+        $em->remove($oferta);
+        $em->flush();
+
+        return $this->redirectToRoute('fos_user_profile_show');
 
     }
 }
