2c9f349fa58e171be274480dde0c06c1d66d1dcb
czifumasa <lukasz.lenart912@gmail.com>
2016-01-06 13:23:44 +0100

    06-01-2015 13:23
    - wyswietlanie zdjec w popularnych
    - slider w popularnych

--- Bootstrap/src/AppBundle/Controller/GlownaController.php
+++ Bootstrap/src/AppBundle/Controller/GlownaController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\SearchEntity;
+use AppBundle\Entity\Oferty;
 use AppBundle\Form\Search;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -25,17 +26,36 @@ class GlownaController extends Controller
         $dql   = "SELECT o FROM AppBundle:Oferty o ORDER BY o.views DESC";
         $query = $em->createQuery($dql);
 
+        //Pobieranie zdjęć
+        $oferty = $query->getResult();
+        $i=0;
+        $zdjecia=null;
+        foreach ($oferty as $of)
+        {
+            $Repository = $this->getDoctrine()
+                ->getRepository('AppBundle:Zdjecia');
+
+            $zdjecia[$i]=$Repository->findOneBy(
+                array('oferta' =>$of)
+            );
+            $i=$i+1;
+        }
+
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
-            5/*limit per page*/
+            8/*limit per page*/
 
         );
+
+
+
 
         return $this->render(':Szablony:glowna.html.twig', array(
             'form' => $form->createView(),
             'pagination' => $pagination,
+            'zdjecia' => $zdjecia
 
 
         ));
