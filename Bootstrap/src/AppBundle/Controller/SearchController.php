9fb5eeda883065a53c8df897e14e12d8cfc2f680
czifumasa <lukasz.lenart912@gmail.com>
2016-01-17 15:01:54 +0100

    17-01-2016 15:01
    -wyposazenie do searcha
    -optymalizacja zapytania do bazy

--- Bootstrap/src/AppBundle/Controller/SearchController.php
+++ Bootstrap/src/AppBundle/Controller/SearchController.php
@@ -44,7 +44,7 @@ class SearchController extends Controller
                 'cenado'=>$cenado,
                 'metrazod'=>$metrazod,
                 'metrazdo'=>$metrazdo,
-                'kategoria'=>$data['kategoria']
+                'kategoria'=>$data['kategoria'],301
             ));
         }
 
@@ -97,53 +97,13 @@ class SearchController extends Controller
             20/*limit per page*/
         );
 
-        //Pobieranie zdjęć//Pobieranie zdjęć
-        $dql2   = "SELECT o FROM AppBundle:Oferty o ORDER BY o.views DESC";
-        $query = $em->createQuery($dql2);
-        $oferty = $query->getResult();
-        $i=0;
-        $zdjecia=null;
-        foreach ($oferty as $of)
-        {
-            $Repository = $this->getDoctrine()
-                ->getRepository('AppBundle:Zdjecia');
 
-            $zdjecia[$i]=$Repository->findOneBy(
-                array('oferta' =>$of)
-            );
-            $i=$i+1;
-        }
-
-        //WYPOSAZENIE
-        $Repository = $this->getDoctrine()
-            ->getRepository('AppBundle:Wyposazenie_Oferty');
-
-        $wyposazenie_oferty=$Repository->findBy(
-            array('oferta' =>$of)
-        );
-
-        $Repository = $this->getDoctrine()
-            ->getRepository('AppBundle:Wyposazenie');
-
-        $i=0;
-        $wyp=null;
-        foreach($wyposazenie_oferty as $wyposazenie)
-        {
-            $wyp[$i] = $Repository->find(
-                $wyposazenie->getWyposazenie()
-            )->getNazwawyposazenia();
-            $i=$i+1;
-        }
 
 
 
         return $this->render(':Szablony:search.html.twig', array(
             'form' => $form->createView(),
             'pagination' => $pagination,
-            'zdjecia' => $zdjecia,
-            'oferty'        => $oferty,
-            'wyposazenie_oferty'        => $wyposazenie_oferty,
-            'wyposazenie'=> $wyp,
         ));
 
     }
